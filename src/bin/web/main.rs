621dd1dcc980766e6e3c8dd6b59c00601a222e66
Billy Stevens <contact@wasv.me>
2020-07-19 17:36:05 -0400

    Adds logging middleware to actix web server

--- src/bin/web/main.rs
+++ src/bin/web/main.rs
@@ -3,18 +3,25 @@ extern crate actix_web;
 extern crate futures;
 extern crate serde_json;
 
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 
 mod api;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    pretty_env_logger::init();
+
     HttpServer::new(|| {
-        App::new().data(api::State::new()).service(
-            web::resource("/inbox")
-                .route(web::post().to(api::inbox::post))
-                .route(web::get().to(api::inbox::get)),
-        )
+        App::new()
+            .wrap(Logger::default())
+            .data(api::State::new())
+            .service(
+                web::resource("/inbox")
+                    .route(web::post().to(api::inbox::post))
+                    .route(web::get().to(api::inbox::get)),
+            )
     })
     .bind("127.0.0.1:8088")?
     .run()
