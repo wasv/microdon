27779e114a0d568bdabca10ab5aeefebef97fbd5
Billy Stevens <contact@wasv.me>
2020-08-02 14:13:04 -0400

    Adds tokio to CLI to simplify codepath.

--- src/bin/cli/main.rs
+++ src/bin/cli/main.rs
@@ -1,37 +1,47 @@
-extern crate clap;
-extern crate diesel;
-extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
 use clap::{App, SubCommand};
-use futures::executor::block_on;
 use microdon::connection;
 use microdon::handlers;
 
-async fn execute_command(subcommand: Option<&str>) -> Result<(), String> {
-    let db = connection::DbConn(connection::init_pool().get().unwrap());
-    let activity = serde_json::from_reader(std::io::stdin().lock()).unwrap();
-
-    match subcommand {
-        Some("inbox") => handlers::inbox::create(db, activity).await.map(|a| {
-            info!("Activity:\n {:#?}", a);
-        }),
-        Some("outbox") => handlers::outbox::create(db, activity).await.map(|a| {
-            info!("Activity:\n {:#?}", a);
-        }),
-        _ => Err("Invalid command.".to_string()),
-    }
-}
-
-fn main() {
+#[tokio::main]
+async fn main() {
     pretty_env_logger::init();
     let args = App::new("microdon")
         .subcommands(vec![
-            SubCommand::with_name("inbox").about("Add activity to inbox"),
-            SubCommand::with_name("outbox").about("Add activity to outbox"),
+            SubCommand::with_name("create")
+                .about("Create an acitivity from stdin")
+                .subcommands(vec![
+                    SubCommand::with_name("inbox").about("Add activity to inbox"),
+                    SubCommand::with_name("outbox").about("Add activity to outbox"),
+                ]),
+            SubCommand::with_name("list").about("Lists all known objects"),
         ])
         .get_matches();
 
-    block_on(execute_command(args.subcommand_name())).unwrap_or_else(|e| error!("{}", e))
+    let db = connection::DbConn(connection::init_pool().get().unwrap());
+
+    match args.subcommand() {
+        ("create", Some(subargs)) => {
+            let activity = serde_json::from_reader(std::io::stdin().lock()).unwrap();
+
+            match subargs.subcommand_name() {
+                Some("inbox") => handlers::inbox::create(db, activity).await.map(|a| {
+                    info!("Activity:\n {:#?}", a);
+                }),
+                Some("outbox") => handlers::outbox::create(db, activity).await.map(|a| {
+                    info!("Activity:\n {:#?}", a);
+                }),
+                _ => Err("Invalid create command.".to_string()),
+            }
+            .unwrap_or_else(|e| error!("{}", e));
+        }
+        ("list", _) => {
+            if let Ok(res) = serde_json::to_string(&handlers::inbox::get_all(db)) {
+                println!("{}", res);
+            }
+        }
+        _ => error!("Invalid command."),
+    }
 }
