d1ecb92aee0c37d4eb95b6e687d0f6ca74fbfb7d
Billy Stevens <contact@wasv.me>
2020-08-02 06:36:46 -0400

    Moves common request handling routines into seperate functions.

--- src/bin/web/api/inbox.rs
+++ src/bin/web/api/inbox.rs
@@ -1,5 +1,4 @@
 use actix_web::{error, web, Responder};
-use futures::StreamExt;
 
 use super::State;
 
@@ -7,19 +6,11 @@ use microdon::connection::DbConn;
 use microdon::handlers::inbox;
 use microdon::models::Activity;
 
-pub async fn post(mut body_stream: web::Payload, state: web::Data<State>) -> impl Responder {
-    let mut body = web::BytesMut::new();
-    while let Some(item) = body_stream.next().await {
-        body.extend_from_slice(&item?);
-    }
-    let body = body.freeze();
+pub async fn post(body_stream: web::Payload, state: web::Data<State>) -> impl Responder {
+    let activity = super::parse_body(body_stream).await?;
+    let db = state.get_db();
 
-    let db = DbConn(state.db.get().unwrap());
-
-    let json_act = serde_json::from_slice(&body[..])
-        .map_err(|e| error::ErrorBadRequest(format!("JSON Error {}", e)))?;
-
-    inbox::create(db, json_act)
+    inbox::create(db, activity)
         .await
         .map_err(|e| error::ErrorInternalServerError(format!("Create failed {}", e)))
         .map(web::Json)
--- src/bin/web/api/mod.rs
+++ src/bin/web/api/mod.rs
@@ -1,6 +1,9 @@
 pub mod inbox;
 
+use actix_web::{error, web};
+use futures::StreamExt;
 use microdon::connection;
+use microdon::connection::DbConn;
 
 pub struct State {
     db: connection::Pool,
@@ -12,4 +15,19 @@ impl State {
             db: connection::init_pool(),
         }
     }
+
+    pub fn get_db(&self) -> DbConn {
+        DbConn(self.db.get().unwrap())
+    }
+}
+
+pub async fn parse_body(mut stream: web::Payload) -> Result<serde_json::Value, actix_web::Error> {
+    let mut body = web::BytesMut::new();
+    while let Some(item) = stream.next().await {
+        body.extend_from_slice(&item?);
+    }
+    let body = body.freeze();
+
+    serde_json::from_slice(&body[..])
+        .map_err(|e| error::ErrorBadRequest(format!("JSON Error {}", e)))
 }
